d68aa012cfa755c8db3cd48f2b5c24a04bfebe6e
Simey de Klerk <simeydeklerk@gmail.com>
2020-11-22 15:42:03 +0200

    Input output design WIP

--- src/components/fileupload.js
+++ src/components/fileupload.js
@@ -1,44 +1,62 @@
 import React, { useCallback, useState } from 'react'
-import FileUploadStyles from "./fileupload.module.css"
 import { useDropzone } from 'react-dropzone'
+
+import path from 'path'
+
+import styles from "./fileupload.module.css"
+
+function sortByName(obj1, obj2) {
+    return obj1.name.localeCompare(obj2.name);
+}
+
+const ipcRenderer = window.require("electron").ipcRenderer;
 
 function FileUpload() {
     const [message, setMessage] = useState('Drag and drop some files here, or click to select the pdfs you want to combine.')
+    const [inputFiles, setInputFiles] = useState([]);
+    const [outputFile, setOutputFile] = useState('');
+
     const onDrop = useCallback(acceptedFiles => {
-        var paths = []
 
-        function sortByName(obj1, obj2) {
-            return obj1.name.localeCompare(obj2.name);
-        }
-        const sortedFiles = acceptedFiles.sort(sortByName) // sorted alphabetically by name
-
-        sortedFiles.map(file =>
-            paths.push(file.path)
-        )
-        const ipcRenderer = window.require("electron").ipcRenderer;
-
-        if (paths.length === 0) {
-            setMessage('You dropped an invalid filetype 🚫')
-        }
-        else if (paths.length === 1) {
-            setMessage('Did you really just try to combine a single file? 🤨')
-        }
-        else {
-            setMessage('Working on it ⚙️')
-            ipcRenderer.send("merge", paths)
-        }
+        const paths = acceptedFiles.sort(sortByName)
+        setInputFiles(paths)
     }, [])
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept: 'application/pdf' })
 
     return (
-        <div {...getRootProps()} className={FileUploadStyles.wrapper}>
-            <input {...getInputProps()} className={FileUploadStyles.input} />
-            {
-                isDragActive ?
-                    <p className={FileUploadStyles.dropIt}>Drop it like it's hot 🔥</p> :
-                    <p>{message}</p>
-            }
+        <div className={styles.wrapper} >
+            <div className={styles.input_area}>
+                <h2 className={styles.title}>Input Files</h2>
+                <div {...getRootProps()} className={styles.dropzone}>
+                    <input {...getInputProps()} className={styles.input} />
+                    <ol className={styles.file_list}>
+                        {inputFiles.map(x => <li className={styles.file_item}>{x.name}</li>)}
+                    </ol>
+                    <div className={styles.drop_outer}>
+                        <div className={styles.drop_inner}>
+                            {
+                                isDragActive ?
+                                    <p className={styles.dropIt}>Drop it like it's hot 🔥</p> :
+                                    <p>{message}</p>
+                            }
+                        </div>
+
+                    </div>
+                    <ul>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.output_area}>
+                <h2 className={styles.title}>Output File</h2>
+                <div className={styles.output_form}>
+                    <div className={styles.input_group}>
+                        <input type="text" className={styles.textbox} value={outputFile} onChange={e => setOutputFile(e.target.value)} />
+                        <button className={styles.browse_button}>...</button>
+                    </div>
+                    <button className={styles.go_button}>Go</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/titlebar.js
+++ src/components/titlebar.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import TitlebarStyles from './titlebar.module.css'
+import styles from './titlebar.module.css'
 import { VscChromeClose, VscChromeMinimize } from 'react-icons/vsc'
 
 function Titlebar() {
     const ipcRenderer = window.require("electron").ipcRenderer;
 
     return (
-        <div id="title-bar" className={TitlebarStyles.titleBar}>
-            <div id="title" className={TitlebarStyles.title}>
+        <div id="title-bar" className={styles.titleBar}>
+            <div id="title" className={styles.title}>
                 <p>
                     Fusion
                 </p>
             </div>
-            <div id="title-bar-btns" className={TitlebarStyles.btns}>
-                <div id="min-button" className={TitlebarStyles.btn}>
+            <div id="title-bar-btns" className={styles.btns}>
+                <div id="min-button" className={styles.btn}>
                     <VscChromeMinimize onClick={(e) => ipcRenderer.send("minimize-window")} />
                 </div>
-                <div id="close-button" className={TitlebarStyles.btn} >
+                <div id="close-button" className={styles.btn} >
                     <VscChromeClose onClick={(e) => ipcRenderer.send("close-window")} />
                 </div>
             </div>
